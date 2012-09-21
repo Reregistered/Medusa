1add2392e9b8a1a4617525597002b9cd2fc9cc19
Dov Amihod <dov@getTeamUp.com>
2012-09-21 15:02:51 -0400

    update to fix a broken command line.

--- server.js
+++ server.js
@@ -3,29 +3,18 @@
 // The UI was inspired from kue
 
 var path = require('path')
-  , program = require('commander')
+  , args = require("optimist").usage('Usage: $0 ')
+    .default('host',   '127.0.0.1')
+    .default('port',   '6379')
+    .default('server', '8010')
+    .argv;
 
 /////////////////////////////////////////////////
 // we expect that the redis server and port
 // will be passed on the command line.
 
-var redisIP = '127.0.0.1';
-var redisPort = '6379';
-var serverPort = 8010;
-
-program
-  .version('0.0.1')
-  .option('-h, --host', 'Redis Host (Defaults to ' + redisIP + ')')
-  .option('-p, --port', 'Redis port ( Defaults to ' + redisPort + ')')
-  .option('-s, --server', 'Server Port ( Defaults to ' + serverPort +')')
-  .parse(process.argv);
-
-redisIP = program.host || redisIP;
-redisPort = program.port || redisPort;
-serverPort = program.server || serverPort;
-
 app = require(path.join(__dirname,'lib','http'));
 
-app.init({host:redisIP ,port:redisPort});
+app.init({host:args.host,port:args.port});
 
-app.listen(serverPort);
+app.listen(args.server);
